51bc9a43d70c6b9c770181d85a541a574b460ff9
GabiK - Win <gabi@kazav.net>
2016-05-15 22:47:37 +0300

    wear

---
+++ RemoteDoor/remotedoorwear/src/main/java/net/kazav/gabi/remotedoorwear/main.java
@@ -0,0 +1,13 @@
+package net.kazav.gabi.remotedoorwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.round_activity_main);
+    }
+}
